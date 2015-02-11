dfcbc940cd65dd4b8b2726775de635f64cbbd76f
Liam Young <liam.young@canonical.com>
2015-02-11 15:51:08 +0000

    Add more tests

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -63,9 +63,6 @@ class OpenstackDashboardBasicDeployment(OpenStackAmuletDeployment):
         self.keystone_sentry = self.d.sentry.unit['keystone/0']
         self.openstack_dashboard_sentry = self.d.sentry.unit['openstack-dashboard/0']
 
-        # Let things settle a bit before moving forward
-        time.sleep(30)
-
     def test_services(self):
         """Verify the expected services are running on the corresponding
            service units."""
@@ -80,17 +77,7 @@ class OpenstackDashboardBasicDeployment(OpenStackAmuletDeployment):
         if ret:
             amulet.raise_status(amulet.FAIL, msg=ret)
 
-    def test_local_settings(self):
-        unit = self.openstack_dashboard_sentry
-        ksentry = self.keystone_sentry
-        conf = '/etc/openstack-dashboard/local_settings.py'
-        file_contents = unit.file_contents(conf)
-        rdata = ksentry.relation('identity-service', 'openstack-dashboard:identity-service')
-        expected = {
-            'LOGIN_REDIRECT_URL': """'/horizon'""",
-            'OPENSTACK_HOST': '"%s"' % (rdata['private-address']),
-            'OPENSTACK_KEYSTONE_DEFAULT_ROLE': '"Member"'
-        }
+    def crude_py_parse(self, file_contents, expected):
         for line in file_contents.split('\n'):
             if '=' in line:
                  args = line.split('=')
@@ -103,6 +90,29 @@ class OpenstackDashboardBasicDeployment(OpenStackAmuletDeployment):
                          msg="Mismatch %s != %s" % (expected[key], value)
                          amulet.raise_status(amulet.FAIL, msg=msg)
 
+
+    def test_local_settings(self):
+        unit = self.openstack_dashboard_sentry
+        ksentry = self.keystone_sentry
+        conf = '/etc/openstack-dashboard/local_settings.py'
+        file_contents = unit.file_contents(conf)
+        rdata = ksentry.relation('identity-service', 'openstack-dashboard:identity-service')
+        expected = {
+            'LOGIN_REDIRECT_URL': """'/horizon'""",
+            'OPENSTACK_HOST': '"%s"' % (rdata['private-address']),
+            'OPENSTACK_KEYSTONE_DEFAULT_ROLE': '"Member"'
+        }
+        self.crude_py_parse(file_contents, expected)
+
+    def test_router_settings(self):
+        unit = self.openstack_dashboard_sentry
+        conf = '/usr/share/openstack-dashboard/openstack_dashboard/enabled/_40_router.py'
+        file_contents = unit.file_contents(conf)
+        expected = {
+            'DISABLED': "True",
+        }
+        self.crude_py_parse(file_contents, expected)
+
     def test_connection(self):
         unit = self.openstack_dashboard_sentry
         dashboard_relation = unit.relation('identity-service',
@@ -113,3 +123,57 @@ class OpenstackDashboardBasicDeployment(OpenStackAmuletDeployment):
         if 'OpenStack Dashboard' not in html:
             msg="Dashboard frontpage check failed"
             amulet.raise_status(amulet.FAIL, msg=msg)
+
+    def test_z_restart_on_config_change(self):
+        """Verify that the specified services are restarted when the config
+           is changed.
+
+           Note(coreycb): The method name with the _z_ is a little odd
+           but it forces the test to run last.  It just makes things
+           easier because restarting services requires re-authorization.
+           """
+        conf = '/etc/openstack-dashboard/local_settings.py'
+        services = ['apache2']
+        self.d.configure('openstack-dashboard', {'use-syslog': 'True'})
+        time = 20
+        for s in services:
+            if not u.service_restarted(self.openstack_dashboard_sentry, s, conf,
+                                       pgrep_full=True, sleep_time=time):
+                self.d.configure('openstack-dashboard', {'use-syslog': 'False'})
+                msg = "service {} didn't restart after config change".format(s)
+            time = 0
+        self.d.configure('openstack-dashboard', {'use-syslog': 'False'})
+
+    def test_openstack_dashboard_identity_service_relation(self):
+        """Verify the openstack-dashboard to keystone identity-service relation data."""
+        unit = self.openstack_dashboard_sentry
+        relation = ['identity-service', 'keystone:identity-service']
+        expected = {
+            'private-address': u.valid_ip,
+            'requested_roles': 'Member',
+        }
+
+        ret = u.validate_relation_data(unit, relation, expected)
+        if ret:
+            message = u.relation_error('openstack-dashboard identity-service', ret)
+            amulet.raise_status(amulet.FAIL, msg=message)
+
+    def test_keystone_identity_service_relation(self):
+        """Verify the keystone to openstack-dashboard identity-service relation data."""
+        unit = self.keystone_sentry
+        relation = ['identity-service', 'openstack-dashboard:identity-service']
+        expected = {
+            'auth_host': u.valid_ip,
+            'auth_port': '35357',
+            'auth_protocol': 'http',
+            'private-address': u.valid_ip,
+            'region': 'RegionOne',
+            'service_host': u.valid_ip,
+            'service_port': '5000',
+            'service_protocol': 'http',
+        }
+
+        ret = u.validate_relation_data(unit, relation, expected)
+        if ret:
+            message = u.relation_error('keystone identity-service', ret)
+            amulet.raise_status(amulet.FAIL, msg=message)
